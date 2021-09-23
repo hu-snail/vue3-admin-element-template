5d20f2b7983f08c5dd1b3889a04e5170f8c8d8bc
jiangjun123 <1217437592@qq.com>
2021-09-23 21:18:37 +0800

    dist

--- dist/static/index-legacy.ba9ae009.js
+++
@@ -1,1 +0,0 @@
-System.register(["./vendor-legacy.3827e8e0.js"],(function(e){"use strict";var r,t,n;return{setters:[function(e){r=e.r,t=e.o,n=e.e}],execute:function(){e("default",{}).render=function(e,u){var c=r("router-view");return t(),n(c)}}}}));
