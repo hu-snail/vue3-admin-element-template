f717321430dfc08808784c96936bedb55762eef7
jiangjun123 <1217437592@qq.com>
2021-09-23 21:20:31 +0800

    dist

---
+++ dist/static/index-legacy.ba9ae009.js
@@ -0,0 +1,1 @@
+System.register(["./vendor-legacy.3827e8e0.js"],(function(e){"use strict";var r,t,n;return{setters:[function(e){r=e.r,t=e.o,n=e.e}],execute:function(){e("default",{}).render=function(e,u){var c=r("router-view");return t(),n(c)}}}}));
